625659d00b4e8df8651af9cc81136c6bf48e404e
Diego Lehyt <diegolehy00@gmail.com>
2020-06-29 12:30:07 -0400

    Home page, Navbar and wrapper added

---
+++ portfolio/src/components/Navbar/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./style.css";
+
+const styles = {
+  navB: {
+    fontFamily: "'Trade Winds', cursive"
+  },
+  imgB: {
+    marginRight: "10px"
+  }
+}
+
+// Depending on the current path, this component sets the "active" className on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+      <div className="container">
+        <a className="navbar-brand" href="index.html" style={styles.navB}><img src="images/logo.png" width="40px" height="40px" tabindex="-1" style={styles.imgB} alt="logo"/>Diego Lehyt</a>
+
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+          <ul className="navbar-nav mr-auto smooth-scroll">
+            <li className="nav-item">
+              <a className="nav-link" href="about.html" data-offset="90">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="portfolio.html" data-offset="90">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="contact.html" data-offset="90">Contact</a>
+            </li>
+          </ul>
+
+          {/* <!-- Social Icon  --> */}
+          <ul className="navbar-nav nav-flex-icons">
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.linkedin.com/in/diego-lehyt-1616b11a6/">
+                <i className="fab fa-linkedin-in light-green-text-2"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://github.com/diegolehyt">
+                <i className="fab fa-github light-green-text-2"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.docdroid.net/4B2tXwf/diegoresume-pdf">
+                <i className="far fa-address-card light-green-text-2"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.instagram.com/diego.lehyt/?hl=es">
+                <i className="fab fa-instagram light-green-text-2"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.facebook.com/Diego.Lehyt">
+                <i className="fab fa-facebook-f light-green-text-2"></i>
+              </a>
+            </li>
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+
+  );
+}
+
+export default Navbar;
---
+++ portfolio/src/pages/Home.js
@@ -0,0 +1,60 @@
+import React from "react";
+// import Hero from "../components/Hero";
+
+const styles = {
+  headerB: {
+    fontFamily: "'Architects Daughter', cursive" 
+  },
+  headerC: {
+    fontFamily: "'Trade Winds', cursive" 
+  },
+  view: {
+    height: '100%'
+  }
+}
+
+function Home() {
+  return (
+    <div id="intro-section" className="view" style={styles.view}>
+        
+      <video className="video-intro" poster="https://mdbootstrap.com/img/Photos/Others/background.jpg" playsInline
+        autoPlay muted loop>
+        <source src="https://static.videezy.com/system/resources/previews/000/009/156/original/fvbhd0229.mp4" type="video/mp4"/>
+      </video>
+
+
+      <div className="mask rgba-gradient d-flex justify-content-center align-items-center">
+
+
+        <div className="container px-md-3 px-sm-0 animated fadeInRight">
+
+
+          <div className="row wow fadeIn">
+
+
+            <div className="col-md-12 mb-4 white-text text-center wow fadeIn">
+
+              <h3 style={styles.headerC} className="display-3 font-weight-bold white-text mb-0 pt-md-5 pt-5">Diego Lehyt</h3>
+              <hr className="hr-light my-4 w-75 style-two"/>
+              <h4 style={styles.headerB} className="subtext-header mt-2 mb-4">Creative Full-Stack web developer, ready to work with you.</h4>
+              <a href="portfolio.html" className="btn btn-rounded btn-outline-white">
+                <i className="fas fa-briefcase"></i> My work
+              </a>
+
+            </div>
+        
+
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default Home;
