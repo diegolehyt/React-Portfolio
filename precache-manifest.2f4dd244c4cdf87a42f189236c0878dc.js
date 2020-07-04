657a4db0eec1bb9fb9336bf0a3e1ecc03a35382b
Diego Lehyt <diegolehy00@gmail.com>
2020-07-04 13:09:40 -0400

    Updates

---
+++ precache-manifest.2f4dd244c4cdf87a42f189236c0878dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c46966387cb9408d93cff5d822c42c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "4908cdc710863902f832",
+    "url": "/React-Portfolio/static/css/main.344ecbd1.chunk.css"
+  },
+  {
+    "revision": "63468856413ad9841a9a",
+    "url": "/React-Portfolio/static/js/2.9a4e0040.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.9a4e0040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4908cdc710863902f832",
+    "url": "/React-Portfolio/static/js/main.f2afc56c.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  }
+]);
